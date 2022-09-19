506a340b8dc33052b47e97e85d3bf81245075e7e
Noé Fontana <33232437+NoeFontana@users.noreply.github.com>
2022-09-18 22:01:14 -0400

    Conf mypy 2 (#2)
    * wip auto formatter not working on vscode
    
    * autopep 8
    
    * improve lint workflow
    
    * Adds precommit hooks via setup
    
    * add pip-tools and basic packages
    
    * Add main with dummy policy
    
    * needs to fix mypy conf
    
    * more robust dummy command
    
    * cleanup repo
    
    * fix state accessor
    
    * fix mypy config
    
    * enable mypy again

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
+import time
 
+import pybullet as p
 
-def main() -> None:
-    print('Hello world')
-
+import src.utils.constants as constants
+from src.compute_command.compute_command import dummy_command
+from src.utils.create_simulation import create_simulation
 
 if __name__ == '__main__':
-    main()
+    client, robot = create_simulation()
+    # set the center of mass frame (loadURDF sets base link frame)
+    # startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos,
+    # startOrientation)
+    for i in range(constants.MAX_SIMULATION_STEPS):
+        if (i % constants.COMMAND_PERIOD_STEPS) == 0:
+            joint_positions = robot.get_positions()
+            command = dummy_command(joint_positions)
+            robot.send_command(command)
+        p.stepSimulation()
+        time.sleep(constants.TIMESTEP)
+    p.disconnect()
